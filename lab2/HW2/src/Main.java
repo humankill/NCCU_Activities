8353db428fc461833873242e7f613fd7bc7cfe84
lina2360 <a84111011@gmail.com>
2021-10-04 10:39:43 +0800

    1004

--- lab2/HW2/src/Main.java
+++ lab2/HW2/src/Main.java
@@ -6,38 +6,45 @@ public class Main {
 		
 		System.out.println("Please type (1)type and (2)number of people or initial capital and (3)years");
 		Scanner sc = new Scanner(System.in);
-		
-		while(sc.hasNext()) {
-			int type = sc.nextInt();
-			
-			switch(type){
-			
-				case 0://population
-		//			int people = ; 
-		//			int prate=;
-		//			int pyears = ;
-		//			int generation = ;
-		
-		//			GeomProgression<Integer> Population = new GeomProgression<Integer>(people,prate);
-		//			Population.printProgression(generation+1);
-					break;
-				case 1://capital
-		//			double initial = ; 
-		//			double crate=;
-		//			int cyears = ;
-		//			GeomProgression<Double> Capital = new GeomProgression<Double>(initial, crate);
-		//			Capital.printProgression(cyears+1);
-					break;
-				default:
-					System.out.println("InvalidType please enter type for 0 or 1");
-					//consume the following 3 useless tokens
-					sc.next();
-					sc.next();
-					sc.next();
-					break;
+			try {
+				while(sc.hasNext()) {
+					int type = sc.nextInt();
+					
+					switch(type){
+					
+						case 0://population
+				//			int people = ; 
+							int prate=2;
+				//			int pyears = ;
+				//			int generation = ;
+				
+							GeomProgression<Integer> Population = new GeomProgression<Integer>(people,prate);
+							Population.printProgression(generation+1);
+							break;
+						case 1://capital
+				//			double initial = ; 
+							double crate=1.016;
+				//			int cyears = ;
+							GeomProgression<Double> Capital = new GeomProgression<Double>(initial, crate);
+							Capital.printProgression(cyears+1);
+							break;
+						default:
+							System.out.println("InvalidType please enter type for 0 or 1");
+				
+							break;
+					}
+				}
+				
+				
+			}catch(Exception e) {
+				System.out.println(e);
 			}
-		}
+			
 		sc.close();
 	}
 
 }
+
+//0 2 120
+//1 100 2
+//3

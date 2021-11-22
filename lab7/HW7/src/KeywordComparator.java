cf80b512b7b6ae0dca29146d0e48b95d9ced1717
lna2360 <a84111011@gmail.com>
2021-11-22 11:30:54 +0800

    1122

---
+++ lab7/HW7/src/KeywordComparator.java
@@ -0,0 +1,12 @@
+
+import java.util.Comparator;
+
+public class KeywordComparator implements Comparator<Keyword>{
+	@Override
+	public int compare(Keyword o1, Keyword o2){
+		if(o1==null || o2==null) throw new NullPointerException();
+		//1. compare
+		
+		return 0;
+	}
+}
